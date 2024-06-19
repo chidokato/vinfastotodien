d4b39f1fe1e19d0fa79f3aeaee02c8cecc4af653
chidokato <tuan.pn92@gmail.com>
2024-06-19 16:22:00 +0700

    sdf

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -6,44 +6,26 @@
 @section('url'){{asset('')}}@endsection
 
 @section('css')
-<style type="text/css">
-#sync1 .item{ border:1px solid #ddd; border-radius:5px }
-#sync1 .item img{ border-radius:5px }
-#sync2 .item {
-  padding: 10px 0px;
-  margin: 5px;
-  color: #FFF;
-  -webkit-border-radius: 3px;
-  -moz-border-radius: 3px;
-  border-radius: 3px;
-  text-align: center;
-  cursor: pointer;
-}
-.owl-theme .owl-nav {
-}
-.owl-theme .owl-nav [class*=owl-] {
-  transition: all 0.3s ease;
-}
-.owl-theme .owl-nav [class*=owl-].disabled:hover {
-  background-color: #D6D6D6;
-}
-
-#sync1.owl-theme {
-  position: relative;
-}
-#sync1.owl-theme .owl-next, #sync1.owl-theme .owl-prev {
-  width: 22px;
-  height: 40px;
-  margin-top: -20px;
-  position: absolute;
-  top: 50%;
-}
-#sync1.owl-theme .owl-prev {
-  left: 10px;
-}
-#sync1.owl-theme .owl-next {
-  right: 10px;
-}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+<style>
+    .owl-item img {
+        width: 100%;
+        height: auto;
+    }
+    .thumbnails {
+        display: flex;
+        justify-content: center;
+        margin-top: 10px;
+    }
+    .thumbnails img {
+        width: 100px;
+        height: auto;
+        cursor: pointer;
+        margin: 0 5px;
+    }
+    .thumbnails img.active {
+        border: 2px solid #000;
+    }
 </style>
 @endsection
 
@@ -73,7 +55,7 @@
         <div class="row">
             <div class="col-lg-7 col-md-7">
                 <div class="product-details-tab">
-                    <div id="sync1" class="owl-carousel owl-theme">
+                    <!-- <div id="sync1" class="owl-carousel owl-theme">
                         <div class="item">
                             <img src="data/product/{{$post->img}}">
                         </div>
@@ -82,17 +64,20 @@
                             <img src="data/product/detail/{{$val->img}}" alt="">
                         </div>
                         @endforeach
-                    </div>
-                    <div id="sync2" class="owl-carousel owl-theme">
-                        <div class="item">
-                            <img src="data/product/{{$post->img}}">
-                        </div>
+                    </div> -->
+                    <div class="main-carousel owl-carousel">
+                        <div> <img src="data/product/{{$post->img}}" alt="Image 1"> </div>
                         @foreach($images as $val)
-                        <div class="item">
-                            <img src="data/product/detail/{{$val->img}}" alt="">
-                        </div>
+                        <div> <img src="data/product/detail/{{$val->img}}" alt="Image 1"> </div>
                         @endforeach
                     </div>
+                    <div class="thumbnail-carousel owl-carousel">
+                        <img src="data/product/{{$post->img}}" alt="Thumbnail 1" data-index="0">
+                        @foreach($images as $key => $val)
+                        <img src="data/product/detail/{{$val->img}}" alt="Thumbnail 1" data-index="{{$key+1}}">
+                        @endforeach
+                    </div>
+                
                 </div>
             </div>
 
@@ -255,86 +240,41 @@ segments: segments
 // tạo menu tự động từ h2 h3
 </script>
 
-<script type="text/javascript">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+
+<script>
     $(document).ready(function() {
+        var mainCarousel = $(".main-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            nav: true,
+            dots: false
+        });
 
-    var sync1 = $("#sync1");
-    var sync2 = $("#sync2");
-    var slidesPerPage = 4; //globaly define number of elements per page
-    var syncedSecondary = true;
-
-    sync1.owlCarousel({
-        items: 1,
-        slideSpeed: 2000,
-        nav: true,
-        autoplay: false, 
-        dots: false,
-        loop: false,
-        responsiveRefreshRate: 200,
-        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    }).on('changed.owl.carousel', syncPosition);
-
-    sync2
-        .on('initialized.owl.carousel', function() {
-            sync2.find(".owl-item").eq(0).addClass("current");
-        })
-        .owlCarousel({
-            items: slidesPerPage,
-            dots: false,
+        var thumbnailCarousel = $(".thumbnail-carousel").owlCarousel({
+            items: 4,
+            loop: false,
+            margin: 10,
             nav: false,
-            smartSpeed: 200,
-            slideSpeed: 500,
-            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
-            responsiveRefreshRate: 100
-        }).on('changed.owl.carousel', syncPosition2);
+            dots: false
+        });
 
-    function syncPosition(el) {
-        //if you set loop to false, you have to restore this next line
-        //var current = el.item.index;
+        $(".thumbnail-carousel .owl-item").on("click", function() {
+            var index = $(this).find("img").data("index");
+            mainCarousel.trigger("owl.goTo", index);
+            $(".thumbnail-carousel .owl-item").removeClass("active");
+            $(this).addClass("active");
+        });
 
-        //if you disable loop you have to comment this block
-        var count = el.item.count - 1;
-        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
-
-        if (current < 0) {
-            current = count;
-        }
-        if (current > count) {
-            current = 0;
-        }
-
-        //end block
-
-        sync2
-            .find(".owl-item")
-            .removeClass("current")
-            .eq(current)
-            .addClass("current");
-        var onscreen = sync2.find('.owl-item.active').length - 1;
-        var start = sync2.find('.owl-item.active').first().index();
-        var end = sync2.find('.owl-item.active').last().index();
-
-        if (current > end) {
-            sync2.data('owl.carousel').to(current, 100, true);
-        }
-        if (current < start) {
-            sync2.data('owl.carousel').to(current - onscreen, 100, true);
-        }
-    }
-
-    function syncPosition2(el) {
-        if (syncedSecondary) {
-            var number = el.item.index;
-            sync1.data('owl.carousel').to(number, 100, true);
-        }
-    }
-
-    sync2.on("click", ".owl-item", function(e) {
-        e.preventDefault();
-        var number = $(this).index();
-        sync1.data('owl.carousel').to(number, 300, true);
+        mainCarousel.on("changed.owl.carousel", function(event) {
+            var index = event.item.index;
+            $(".thumbnail-carousel .owl-item").removeClass("active");
+            $(".thumbnail-carousel .owl-item").eq(index).addClass("active");
+            var thumbnailIndex = $(".thumbnail-carousel .owl-item.active").index();
+            thumbnailCarousel.trigger("owl.goTo", thumbnailIndex - 1);
+        });
     });
-});
 </script>
 
 @endsection
