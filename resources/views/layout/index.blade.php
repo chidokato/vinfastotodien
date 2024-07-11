4afa0368f793a6028f63a9cd8db1be8aa1228096
chidokato <tuan.pn92@gmail.com>
2024-07-11 15:36:01 +0700

    sdf

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -86,57 +86,59 @@
     @include('layout.footer')
 
 
-    <div class="hotline-phone-ring-wrap form-ring-wrap">
-        <div class="hotline-phone-ring ">
-            <div class="hotline-phone-ring-circle"></div>
-            <div class="hotline-phone-ring-circle-fill"></div>
-            <div class="hotline-phone-ring-img-circle click_popup">
-                <a href="javascript:void(0)" class="pps-btn-img">
-                    <img src="assets/img/icon/dowload.png" alt="Gọi điện thoại" width="50">
+        <div class="hotline-phone-ring-wrap form-ring-wrap">
+            <div class="hotline-phone-ring ">
+                <div class="hotline-phone-ring-circle"></div>
+                <div class="hotline-phone-ring-circle-fill"></div>
+                <div class="hotline-phone-ring-img-circle click_popup">
+                    <a href="javascript:void(0)" class="pps-btn-img">
+                        <img src="assets/img/icon/dowload.png" alt="Gọi điện thoại" width="50">
+                    </a>
+                </div>
+            
+            <div class="hotline-bar click_popup">
+                <a href="javascript:void(0)">
+                    <span class="text-hotline">Bảng giá</span>
                 </a>
             </div>
-        </div>
-        <div class="hotline-bar click_popup">
-            <a href="javascript:void(0)">
-                <span class="text-hotline">Bảng giá</span>
-            </a>
-        </div>
-    </div>
-
-    <div class="hotline-phone-ring-wrap zalo-ring-wrap">
-        <div class="hotline-phone-ring">
-            <div class="hotline-phone-ring-circle"></div>
-            <div class="hotline-phone-ring-circle-fill"></div>
-            <div class="hotline-phone-ring-img-circle">
-            <a target="_blank" href="https://zalo.me/{{$setting->hotline}}" class="pps-btn-img">
-                <img src="assets/img/icon/zalo.png" alt="Gọi điện thoại" width="50">
-            </a>
             </div>
         </div>
-        <div class="hotline-bar">
-            <a target="_blank" href="https://zalo.me/{{$setting->hotline}}">
-                <span class="text-hotline">{{$setting->hotline}}</span>
-            </a>
-        </div>
-    </div>
 
-    <div class="hotline-phone-ring-wrap">
-        <div class="hotline-phone-ring">
-            <div class="hotline-phone-ring-circle"></div>
-            <div class="hotline-phone-ring-circle-fill"></div>
-            <div class="hotline-phone-ring-img-circle">
-            <a href="tel:{{$setting->hotline}}" class="pps-btn-img">
-                <img src="assets/img/icon/icon-call-nh.png" alt="Gọi điện thoại" width="50">
-            </a>
+        <div class="hotline-phone-ring-wrap zalo-ring-wrap">
+            <div class="hotline-phone-ring">
+                <div class="hotline-phone-ring-circle"></div>
+                <div class="hotline-phone-ring-circle-fill"></div>
+                <div class="hotline-phone-ring-img-circle">
+                <a target="_blank" href="https://zalo.me/{{$setting->hotline}}" class="pps-btn-img">
+                    <img src="assets/img/icon/zalo.png" alt="Gọi điện thoại" width="50">
+                </a>
+                </div>
+            
+            <div class="hotline-bar">
+                <a target="_blank" href="https://zalo.me/{{$setting->hotline}}">
+                    <span class="text-hotline">Chat</span>
+                </a>
+            </div>
             </div>
         </div>
-        <div class="hotline-bar">
-            <a href="tel:{{$setting->hotline}}">
-                <span class="text-hotline">{{$setting->hotline}}</span>
-            </a>
+
+        <div class="hotline-phone-ring-wrap hotline-ring-wrap">
+            <div class="hotline-phone-ring">
+                <div class="hotline-phone-ring-circle"></div>
+                <div class="hotline-phone-ring-circle-fill"></div>
+                <div class="hotline-phone-ring-img-circle">
+                <a href="tel:{{$setting->hotline}}" class="pps-btn-img">
+                    <img src="assets/img/icon/icon-call-nh.png" alt="Gọi điện thoại" width="50">
+                </a>
+                </div>
+            
+            <div class="hotline-bar">
+                <a href="tel:{{$setting->hotline}}">
+                    <span class="text-hotline">Gọi ngay</span>
+                </a>
+            </div>
+            </div>
         </div>
-    </div>
-    
     <!-- <div class="fixd-right">
         <ul>
             <li class="click_popup"><a href="javascript:void(0)"><img src="assets/img/icon/pdf.jpg"> <div>Bảng giá</div></a>  </li>
